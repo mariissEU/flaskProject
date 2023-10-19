d5442a8aba217836226531d2daefe17a1aca6617
Maris Liepins <maris.liepins@tietoevry.com>
2023-10-19 09:52:14 +0200

    Added tf output with IP of instance, fixed outputs

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,4 +1,4 @@
 output "maris-ec2-public-ip" {
   description = "The ID of the created EC2 instance"
-  value       = module.maris-test-ec2instance.public_ip
+  value       = module.maris-test-ec2instance.maris-ec2-public-ip
 }
